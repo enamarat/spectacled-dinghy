2148f63c314e959bc0aa48d41f56f063c0d6d692
Glitch (spectacled-dinghy) <none>
2020-05-19 19:10:04 +0000

    🌑😂 Updated with Glitch

--- readme.md
+++
@@ -1,1 +0,0 @@
-A JavaScript app that takes user's input date and returns it in unix and UTC formats.
---
+++ server.js
@@ -0,0 +1,53 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+/********* My solution ****************/
+//specific date is returned based on a user's input
+app.get("/api/timestamp/:date_string", function(req, res) {
+  // if user's input is invalid
+  if (new Date(req.params.date_string).toString() === "Invalid Date") {
+    res.json({
+      error: "Invalid Date"
+    });
+    // if values in user's input are in valid format: "yyyy-mm-dd"
+  } else {
+    res.json({
+      unix: new Date(req.params.date_string).valueOf(),
+      utc: new Date(req.params.date_string.valueOf()).toUTCString(),
+    });
+  }
+});
+
+
+// if no date is specified the current date is returned
+app.get("/api/timestamp/", function(req, res) {
+  res.json({ 
+    // returns number of seconds which have passed since 1 january 1970
+    unix: Date.now(), 
+    // returns Date object
+    utc: Date() 
+  });
+});
+/*************************************************/
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
