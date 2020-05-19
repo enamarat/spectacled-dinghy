2e17e92f4bd47ca729aa414d28829f1b6cc46954
Glitch (spectacled-dinghy) <none>
2020-05-19 19:45:08 +0000

    😅🌷 Updated with Glitch

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ app.get("/", function(req, res) {
 /********* My solution ****************/
 //specific date is returned based on a user's input
 app.get("/api/timestamp/:date_string", function(req, res) {
+   // if user's input is in format: "21213124..." (a bunch of digits), the program will assume it is a UNIX time format
+   const regex = /\d{5,}/;
+   if (regex.test(req.params.date_string)) {
+      res.json({ 
+        unix: req.params.date_string, 
+        utc: new Date(parseInt(req.params.date_string)).toUTCString() 
+     });
+   }
+  
   // if user's input is invalid
   if (new Date(req.params.date_string).toString() === "Invalid Date") {
     res.json({
@@ -28,20 +37,14 @@ app.get("/api/timestamp/:date_string", function(req, res) {
     });
   // if values in user's input are in valid format: "yyyy-mm-dd"
   } else {
-    const regex = /\d{4}-\d{2}-d{2}/;
-    // if user's input is in format: "yyyy-mm-dd"
-    if (regex.test(req.params.date_string)) {
+  // if user's input is in format: "yyyy-mm-dd"
       res.json({
         unix: new Date(req.params.date_string).valueOf(),
         utc: new Date(req.params.date_string.valueOf()).toUTCString(),
       });
-    } 
+    }
     
-    // if user's input is in format: "21213124..." (a bunch of digits), the program will assume it is a UNIX time format
-      res.json({ 
-       unix: req.params.date_string, 
-       utc: new Date(parseInt(req.params.date_string)).toUTCString() 
-     });
+   
     
   }
 });
